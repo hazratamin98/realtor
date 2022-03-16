780985e5433c07631ec9749e7be29aee2575c747
hazratamin98 <hazratamin98@gmail.com>
2022-03-16 19:22:25 +0500

    Footer setup

--- Components/Layout.jsx
+++ Components/Layout.jsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 import { Children } from "react/cjs/react.production.min";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => (
   <>
@@ -8,9 +10,13 @@ const Layout = ({ children }) => (
       <title>Real Estate</title>
     </Head>
     <Box maxWidth="1280px" m="auto">
-      <header>Navbar</header>
+      <header>
+        <Navbar />
+      </header>
       <main>{children}</main>
-      <footer>Footer</footer>
+      <footer>
+        <Footer />
+      </footer>
     </Box>
   </>
 );
