861777f43df9739d6e4fb7f6e8de1a7ea5dc6038
hazratamin98 <hazratamin98@gmail.com>
2022-03-16 16:46:11 +0500

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
+import Property from "../Components/Property";
 
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 
@@ -51,9 +52,9 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
       <Flex flexWrap="wrap">
-        {/* {propertiesForRent.map((property) => (
+        {propertiesForRent.map((property) => (
           <Property property={property} key={property.id} />
-        ))} */}
+        ))}
       </Flex>
       <Banner
         purpose="BUY A HOME"
@@ -66,9 +67,9 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
       <Flex flexWrap="wrap">
-        {/* {propertiesForSale.map((property) => (
+        {propertiesForSale.map((property) => (
           <Property property={property} key={property.id} />
-        ))} */}
+        ))}
       </Flex>
     </Box>
   );
